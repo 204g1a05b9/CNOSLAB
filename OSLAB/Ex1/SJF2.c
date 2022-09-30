f41e89444ef923d60947ce87cc1aa69ab570d255
204g1a05b9 <204g1a05b9@srit.ac.in>
2022-09-30 15:58:42 +0530

    file is created

--- OSLAB/Ex1/SJF2.c
+++ OSLAB/Ex1/SJF2.c
@@ -1,10 +1,7 @@
 #include<stdio.h>
-#include<conio.h>
-main()
+int main()
 {
-int p[20], bt[20], wt[20], tat[20], i, k, n, temp; float wtavg,
-tatavg;
-clrscr();
+int p[20], bt[20], wt[20], tat[20], i, k, n, temp; float wtavg,tatavg;
 printf("\nEnter the number of processes -- ");
 
 scanf("%d", &n);
@@ -26,7 +23,8 @@ p[i]=p[k];
 p[k]=temp;
 }
 wt[0] = wtavg = 0;
-tat[0] = tatavg = bt[0]; for(i=1;i<n;i++)
+tat[0] = tatavg = bt[0]; 
+for(i=1;i<n;i++)
 {
 wt[i] = wt[i-1] +bt[i-1];
 tat[i] = tat[i-1] +bt[i];
@@ -37,4 +35,5 @@ printf("\n\t PROCESS \tBURST TIME \t WAITING TIME\t TURNAROUND TIME\n");
 for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", p[i], bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
-printf("\nAverage Turnaround Time -- %f", tatavg/n); getch();}
+printf("\nAverage Turnaround Time -- %f", tatavg/n); 
+}
